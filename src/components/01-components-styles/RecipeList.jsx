2f5e21ab4827de106a06900a4e0fa95c9763450f
Julie Shapovalenko <jools.shapo@gmail.com>
2023-04-11 11:33:20 +0300

    add recipes files

---
+++ src/components/01-components-styles/RecipeList.jsx
@@ -0,0 +1,8 @@
+export const RecipeList = ({ items }) => {
+    return (<ul>
+        {items.map(item => (
+            <li key={item.id}>{item.name}</li>
+        ))}
+    </ul>
+    );
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { RecipeList } from "./01-components-styles/RecipeList";
+import recipes from "./01-components-styles/recipes.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RecipeList items={recipes} />
+    </>
   );
 };
